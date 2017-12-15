35b3c15ac7d2c01e0ebd9daf5146e4a7622ed9dd
Knut Ahlers <knut@ahlers.me>
2017-12-15 11:31:48 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# Luzifer / slugify
+
+Generate a slug with definable max length from a number of commandline arguments.
+
+```bash
+# ./slugify This should become a slug
+this-should-become-a-slug
+```
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/Luzifer/rconfig"
+	"github.com/gosimple/slug"
+)
+
+var (
+	cfg = struct {
+		MaxLength      int  `flag:"max-length,l" default:"0" description:"Maximum length of returned slug (0 = no limit)"`
+		VersionAndExit bool `flag:"version" default:"false" description:"Prints current version and exits"`
+	}{}
+
+	version = "dev"
+)
+
+func init() {
+	if err := rconfig.Parse(&cfg); err != nil {
+		log.Fatalf("Unable to parse commandline options: %s", err)
+	}
+
+	if cfg.VersionAndExit {
+		fmt.Printf("git-changerelease %s\n", version)
+		os.Exit(0)
+	}
+}
+
+func main() {
+	parts := rconfig.Args()[1:]
+
+	var s string
+	for {
+		s = slug.Make(strings.Join(parts, " "))
+		if cfg.MaxLength == 0 || len(s) < cfg.MaxLength {
+			break
+		}
+
+		parts = parts[0 : len(parts)-1]
+	}
+
+	fmt.Println(s)
+}
